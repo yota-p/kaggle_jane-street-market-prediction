534ae9ded6f0cac8f38d32e835427cfcc8e43495
yota-p <930713yh@gmail.com>
2020-12-13 03:38:15 +0900

    Created directory (#2)
    * added gcs sync
    
    * added comment

--- startup-mlflow.sh
+++ startup-mlflow.sh
@@ -3,5 +3,3 @@
 echo 'Starting mlflow server'
 nohup mlflow server --host 0.0.0.0 & >> mlflow.log 2>&1 &
 
-echo 'Hit below at local to establish port forwarding connection to server:'
-echo '    $ gcloud compute ssh HOST -- -N -L 8080:localhost:8080'
