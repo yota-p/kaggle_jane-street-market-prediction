a695a9118db03e84a4c98d60f21ae88f7ca21d28
yota-p <930713yh@gmail.com>
2020-12-12 10:22:57 +0000

    cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='jane-street-market-prediction',
+    author='yota-p',
+    license='MIT',
+)
