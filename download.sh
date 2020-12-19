1845085a7eeacfd65d3aed1286e1d21ee25175a9
yota-p <930713yh@gmail.com>
2020-12-19 09:17:34 +0000

    feature:data download & gzip

--- download.sh
+++ download.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-kaggle competitions download -c jane-street-market-prediction
-unzip jane-street-market-prediction.zip
+DIR='data/raw'
+kaggle competitions download -c jane-street-market-prediction -p $DIR
+unzip $DIR/jane-street-market-prediction.zip -d $DIR
+
+gzip -c $DIR/features.csv > $DIR/features.gz
+gzip -c $DIR/example_sample_submission.csv > $DIR/example_sample_submission.gz
+gzip -c $DIR/example_test.csv > $DIR/example_test.gz
+gzip -c $DIR/train.csv > $DIR/train.gz
+
+rm $DIR/jane-street-market-prediction.zip
+rm $DIR/*.csv
