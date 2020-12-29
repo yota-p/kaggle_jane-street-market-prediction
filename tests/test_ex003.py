fcddcf3380adc9fc6444239c781b4917b2cf8034
yota-p <930713yh@gmail.com>
2020-12-29 08:04:23 +0000

    fix: wrong assertion

--- tests/test_ex003.py
+++ tests/test_ex003.py
@@ -11,28 +11,32 @@ class TestEx003:
         DATA_DIR = str(tmpdir)
         IN_DIR = f'{DATA_DIR}/002'
         OUT_DIR = f'{DATA_DIR}/{EXNO}'
+        Path(IN_DIR).mkdir(exist_ok=True)
         Path(OUT_DIR).mkdir(exist_ok=True)
 
         # create input
-        Path(IN_DIR).mkdir(exist_ok=True)
         df_in = pd.DataFrame({'key': [1, 2, 3, 4], 'date': [np.int16(0), np.int16(10), np.int16(100), np.int16(101)]})
         df_in.to_pickle(f'{IN_DIR}/train.pkl')
-        for file in ['example_sample_submission', 'example_test', 'features']:
-            f = open(f'{IN_DIR}/{file}.pkl', 'wb')
-            f.write(b'This is {file}')
 
+        copyfiles = ['example_sample_submission.pkl', 'example_test.pkl', 'features.pkl']
+        for file in copyfiles:
+            data = pd.DataFrame({'filename': [file]})
+            data.to_pickle(f'{IN_DIR}/{file}')
+
+        # call target
         mocker.patch('src.ex003_fe_pickle_small.get_datadir', return_value=DATA_DIR)
         ex003_fe_pickle_small.main()
 
+        # assert
         df_actual = pd.read_pickle(f'{OUT_DIR}/train.pkl')
         df_expected = df_in[[True, False, True, False]]
 
         assert df_actual.equals(df_expected)
 
         # assert copied files
-        for file in ['example_sample_submission', 'example_test', 'features']:
-            with open(f'{OUT_DIR}/{file}.pkl', 'rb') as f:
+        for file in copyfiles:
+            with open(f'{IN_DIR}/{file}', 'rb') as f:
                 in_hash = hashlib.sha256(f.read()).hexdigest()
-            with open(f'{OUT_DIR}/{file}.pkl', 'rb') as f:
+            with open(f'{OUT_DIR}/{file}', 'rb') as f:
                 out_hash = hashlib.sha256(f.read()).hexdigest()
             assert(in_hash == out_hash)
