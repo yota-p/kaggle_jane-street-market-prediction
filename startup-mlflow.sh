25724331f6921cd18241f5c497cfda54d672cce3
yota-p <930713yh@gmail.com>
2021-01-12 21:58:13 +0900

    #21 use mlflow (#22)
    * Added make mlflow
    
    * modified startup script
    
    * use mlflow
    
    * Removed unneccesary file
    
    * Changed seq for adding tracking uri
    
    * Fixed mlflow version

--- startup-mlflow.sh
+++ startup-mlflow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'Starting mlflow server'
-nohup mlflow server --host 0.0.0.0 & >> mlflow.log 2>&1 &
+nohup mlflow server --backend-store-uri ./data/mlruns --default-artifact-root ./data/mlruns --host 0.0.0.0  >> mlflow.log 2>&1 &
