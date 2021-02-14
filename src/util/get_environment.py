4424275116fa897f491c65004e507e7586180312
yota-p <930713yh@gmail.com>
2021-02-14 17:36:36 +0000

    reduced deprecated

--- src/util/get_environment.py
+++ src/util/get_environment.py
@@ -2,7 +2,6 @@ import os
 import sys
 import torch
 import hydra
-from argparse import ArgumentParser
 
 
 def get_exec_env() -> str:
@@ -15,69 +14,6 @@ def get_exec_env() -> str:
         return 'colab'
     else:
         return 'local'
-
-
-def parse_args():
-    argparser = ArgumentParser()
-    argparser.add_argument('--small', '-s',
-                           default=False,
-                           action='store_true',
-                           help='Use small data set for debug')
-    argparser.add_argument('--nocache', '-nc',
-                           default=False,
-                           action='store_true',
-                           help='Ignore caches')
-    argparser.add_argument('--notrain', '-nt',
-                           default=False,
-                           action='store_true',
-                           help='Skip training')
-    argparser.add_argument('--nopredict', '-np',
-                           default=False,
-                           action='store_true',
-                           help='Skip prediction')
-    args = argparser.parse_args()
-    return args
-
-
-def parse_env_var_bool(ENV_VAR) -> bool:
-    try:
-        str_env = os.environ[ENV_VAR]
-        if str_env == 'True':
-            return True
-        elif str_env == 'False':
-            return False
-        else:
-            raise ValueError
-    except KeyError:
-        # if not specified in the Notebook, return default value: False
-        return False
-
-
-def get_option():
-    # This function will get execution options
-    # Default values for production: All False
-    option = {
-        'small': False,  # use small data/model/training
-        'nocache': False,  # use cache
-        'notrain': False,  # skip training
-        'nopredict': False  # skip prediction
-    }
-    # To use dev options in ipykernel(approx. Jupyter NB),
-    # you need to specify environment variables.
-    if is_ipykernel():
-        option['small'] = parse_env_var_bool('EXP_SMALL')
-        option['nocache'] = parse_env_var_bool('EXP_NOCACHE')
-        option['notrain'] = parse_env_var_bool('EXP_NOTRAIN')
-        option['nopredict'] = parse_env_var_bool('EXP_NOPREDICT')
-    # To use dev options in cli,
-    # you need to specify commandline arguments.
-    else:
-        args = parse_args()
-        option['small'] = args.small
-        option['nocache'] = args.nocache
-        option['notrain'] = args.notrain
-        option['nopredict'] = args.nopredict
-    return option
 
 
 def is_gpu() -> bool:
@@ -130,6 +66,7 @@ def get_datadir() -> str:
 
 if __name__ == '__main__':
     print(f'Execution environment: {get_exec_env()}')
-    print(f'Get option: {get_option()}')
-    print(f'Is Jupyter Notebook: {is_ipykernel()}')
-    print(f'DATA_DIR: {get_datadir()}')
+    print(f'Is GPU: {is_gpu()}')
+    print(f'Is ipykernel: {is_ipykernel()}')
+    print(f'Original cwd: {get_original_cwd()}')
+    print(f'Datadir: {get_datadir()}')
