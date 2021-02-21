544af30fe172f46ac7f5aab3a570793cc4ccce2f
yota-p <930713yh@gmail.com>
2021-02-21 14:33:48 +0000

    separated train function

--- src/train_v2.py
+++ src/train_v2.py
@@ -280,6 +280,114 @@ def inference_fn(model, dataloader, device, target_cols):
     return preds
 
 
+def train_cv(
+        df: pd.DataFrame,
+        features: List[str],
+        target_cols: List[str],
+        model_name: str,
+        model_param: DictConfig,
+        train_param: DictConfig,
+        cv: DictConfig,
+        OUT_DIR: str,
+        cfg
+        ) -> None:
+
+    if cv.name == 'nocv':
+        print('Training on full data. Note that validation data overlaps train, which will overfit!')
+        train = df
+        valid = df.loc[(df.date >= 450) & (df.date < 500)].reset_index(drop=True)
+    else:
+        train = df.loc[df.date < 450].reset_index(drop=True)
+        valid = df.loc[(df.date >= 450) & (df.date < 500)].reset_index(drop=True)
+
+    NMODELS = cfg.train.param.n_models
+    train_set = MarketDataset(train, features, target_cols)
+    train_loader = DataLoader(train_set, batch_size=cfg.train.param.batch_size, shuffle=True, num_workers=4)
+    valid_set = MarketDataset(valid, features, target_cols)
+    valid_loader = DataLoader(valid_set, batch_size=cfg.train.param.batch_size, shuffle=False, num_workers=4)
+
+    start_time = time.time()
+    for i in range(NMODELS):
+        print(f'Model{i}:')
+        seed_everything(seed=42+i)
+
+        torch.cuda.empty_cache()
+        device = get_device()
+        model = get_model(
+                    model_name=cfg.model.name,
+                    param=cfg.model.param,
+                    feat_cols=features,
+                    target_cols=target_cols,
+                    device=device)
+
+        optimizer = get_optimizer(
+                        optimizer_name=cfg.train.optimizer.name,
+                        param=cfg.train.optimizer.param,
+                        model_param=model.parameters())
+
+        scheduler = get_scheduler(
+                        scheduler_name=cfg.train.scheduler.name,
+                        param=cfg.train.scheduler.param,
+                        steps_per_epoch=len(train_loader),
+                        optimizer=optimizer)
+
+        loss_fn = get_loss_function(
+                    loss_function_name=cfg.train.loss_function.name,
+                    param=cfg.train.loss_function.param)
+
+        es = EarlyStopping(patience=cfg.train.param.early_stopping_rounds, mode='max')
+        for epoch in range(cfg.train.param.epochs):
+            train_loss = train_fn(model, optimizer, scheduler, loss_fn, train_loader, device)
+
+            valid_pred = inference_fn(model, valid_loader, device, target_cols)
+            valid_auc = roc_auc_score(valid[target_cols].values, valid_pred)
+            # valid_logloss = log_loss(valid[target_cols].values, valid_pred)
+            valid_pred = np.median(valid_pred, axis=1)
+            valid_pred = np.where(valid_pred >= 0.5, 1, 0).astype(int)
+            valid_u_score = utility_score_bincount(
+                                date=valid.date.values, weight=valid.weight.values,
+                                resp=valid.resp.values, action=valid_pred)
+            print(f'Model{i} EPOCH:{epoch:3} \
+                train_loss={train_loss:.5f} \
+                valid_u_score={valid_u_score:.5f} \
+                valid_auc={valid_auc:.5f} \
+                time: {(time.time() - start_time) / 60:.2f} min')
+            model_weights = f'{OUT_DIR}/model{i}.pth'
+            es(valid_auc, model, model_path=model_weights)
+            if es.early_stop:
+                print('Early stopping')
+                break
+        # torch.save(model.state_dict(), model_weights)
+
+    # Validate
+    valid_pred = np.zeros((len(valid), len(target_cols)))
+    for i in range(NMODELS):
+        torch.cuda.empty_cache()
+        device = get_device()
+        model = get_model(
+                    model_name=cfg.model.name,
+                    param=cfg.model.param,
+                    feat_cols=features,
+                    target_cols=target_cols,
+                    device=device)
+
+        model_weights = f'{OUT_DIR}/model{i}.pth'
+        model.load_state_dict(torch.load(model_weights))
+
+        valid_pred += inference_fn(model, valid_loader, device, target_cols) / NMODELS
+    auc_score = roc_auc_score(valid[target_cols].values, valid_pred)
+    logloss_score = log_loss(valid[target_cols].values, valid_pred)
+
+    valid_pred = np.median(valid_pred, axis=1)
+    valid_pred = np.where(valid_pred >= 0.5, 1, 0).astype(int)
+    valid_score = utility_score_bincount(
+                    date=valid.date.values, weight=valid.weight.values,
+                    resp=valid.resp.values, action=valid_pred)
+    print(f'{NMODELS} models valid score: {valid_score}\tauc_score: {auc_score:.4f}\tlogloss_score:{logloss_score:.4f}')
+
+    return None
+
+
 @hydra.main(config_path='../config/train_v2', config_name='config')
 def main(cfg: DictConfig) -> None:
     pprint.pprint(dict(cfg))
@@ -330,106 +438,17 @@ def main(cfg: DictConfig) -> None:
     df = crs.fit_transform(df)
     if cfg.feature_engineering.cross.cols is not None:
         all_feat_cols.extend(cfg.feature_engineering.cross.cols)
-
-    if cfg.cv.name == 'nocv':
-        print('Training on full data. Note that validation data overlaps train, which will overfit!')
-        train = df
-        valid = df.loc[(df.date >= 450) & (df.date < 500)].reset_index(drop=True)
-    else:
-        train = df.loc[df.date < 450].reset_index(drop=True)
-        valid = df.loc[(df.date >= 450) & (df.date < 500)].reset_index(drop=True)
-    print(f'Finished feature engineering. train.shape: {train.shape}, valid.shape: {valid.shape}')
+    print(f'Finished feature engineering. train.shape: {df.shape}')
 
     # Train
-    NMODELS = cfg.train.param.n_models
-    train_set = MarketDataset(train, all_feat_cols, target_cols)
-    train_loader = DataLoader(train_set, batch_size=cfg.train.param.batch_size, shuffle=True, num_workers=4)
-    valid_set = MarketDataset(valid, all_feat_cols, target_cols)
-    valid_loader = DataLoader(valid_set, batch_size=cfg.train.param.batch_size, shuffle=False, num_workers=4)
-
     if cfg.option.train:
-        start_time = time.time()
-        for i in range(NMODELS):
-            print(f'Model{i}:')
-            seed_everything(seed=42+i)
-
-            torch.cuda.empty_cache()
-            device = get_device()
-            model = get_model(
-                        model_name=cfg.model.name,
-                        param=cfg.model.param,
-                        feat_cols=all_feat_cols,
-                        target_cols=target_cols,
-                        device=device)
-
-            optimizer = get_optimizer(
-                            optimizer_name=cfg.train.optimizer.name,
-                            param=cfg.train.optimizer.param,
-                            model_param=model.parameters())
-
-            scheduler = get_scheduler(
-                            scheduler_name=cfg.train.scheduler.name,
-                            param=cfg.train.scheduler.param,
-                            steps_per_epoch=len(train_loader),
-                            optimizer=optimizer)
-
-            loss_fn = get_loss_function(
-                        loss_function_name=cfg.train.loss_function.name,
-                        param=cfg.train.loss_function.param)
-
-            es = EarlyStopping(patience=cfg.train.param.early_stopping_rounds, mode='max')
-            for epoch in range(cfg.train.param.epochs):
-                train_loss = train_fn(model, optimizer, scheduler, loss_fn, train_loader, device)
-
-                valid_pred = inference_fn(model, valid_loader, device, target_cols)
-                valid_auc = roc_auc_score(valid[target_cols].values, valid_pred)
-                # valid_logloss = log_loss(valid[target_cols].values, valid_pred)
-                valid_pred = np.median(valid_pred, axis=1)
-                valid_pred = np.where(valid_pred >= 0.5, 1, 0).astype(int)
-                valid_u_score = utility_score_bincount(date=valid.date.values, weight=valid.weight.values,
-                                                       resp=valid.resp.values, action=valid_pred)
-                print(f'Model{i} EPOCH:{epoch:3} \
-                    train_loss={train_loss:.5f} \
-                    valid_u_score={valid_u_score:.5f} \
-                    valid_auc={valid_auc:.5f} \
-                    time: {(time.time() - start_time) / 60:.2f} min')
-                model_weights = f'{OUT_DIR}/model{i}.pth'
-                es(valid_auc, model, model_path=model_weights)
-                if es.early_stop:
-                    print('Early stopping')
-                    break
-            # torch.save(model.state_dict(), model_weights)
-
-        # Validate
-        valid_pred = np.zeros((len(valid), len(target_cols)))
-        for i in range(NMODELS):
-            torch.cuda.empty_cache()
-            device = get_device()
-            model = get_model(
-                        model_name=cfg.model.name,
-                        param=cfg.model.param,
-                        feat_cols=all_feat_cols,
-                        target_cols=target_cols,
-                        device=device)
-
-            model_weights = f'{OUT_DIR}/model{i}.pth'
-            model.load_state_dict(torch.load(model_weights))
-
-            valid_pred += inference_fn(model, valid_loader, device, target_cols) / NMODELS
-        auc_score = roc_auc_score(valid[target_cols].values, valid_pred)
-        logloss_score = log_loss(valid[target_cols].values, valid_pred)
-
-        valid_pred = np.median(valid_pred, axis=1)
-        valid_pred = np.where(valid_pred >= 0.5, 1, 0).astype(int)
-        valid_score = utility_score_bincount(date=valid.date.values, weight=valid.weight.values,
-                                             resp=valid.resp.values, action=valid_pred)
-        print(f'{NMODELS} models valid score: {valid_score}\tauc_score: {auc_score:.4f}\tlogloss_score:{logloss_score:.4f}')
+        train_cv(df, all_feat_cols, target_cols, cfg.model.name, cfg.model.param, cfg.train.param, cfg.cv, OUT_DIR, cfg)
 
     # Predict
     if cfg.option.predict:
         device = get_device()
         model_list = []
-        for i in range(NMODELS):
+        for i in range(cfg.train.param.n_models):
             torch.cuda.empty_cache()
             model = get_model(
                         model_name=cfg.model.name,
